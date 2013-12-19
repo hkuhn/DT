89df3034654930da0ed114100092db59680e3e36
Hobey Kuhn <hkuhn@umich.edu>
2013-12-18 20:14:20 -0500

    writing the load training data script. data is retrieved via google finance

---
+++ src/Data_Processing/load_training_data.sh
@@ -0,0 +1,20 @@
+# Title:
+#    load_training_data.sh
+#
+# Desc: 
+#    The following script sets up the training of the deep belief network by retrieving
+#    data from google straight from a curl command, and then parses it into pickled format
+#    ready to be inputted into the DBN
+#
+# Example:
+#    sh load_training_data.sh
+#
+#
+
+
+DATA_DIR='../data'
+
+
+
+
+
